d802cb9226060e6abe8a454d727220306e64ba6a
Stewart Leach <stewart.leach@isobar.com>
2020-10-30 14:27:25 +1100

    parsing multiple exported assets and using a deep copy

--- src/commands/xml/export-canto-csv.js
+++ src/commands/xml/export-canto-csv.js
@@ -43,7 +43,7 @@ class ExportCantoCsvCommand extends BaseCommand {
         const {
             log: logFile,
             output: htmlResult,
-            inputcsv,
+            inputxml,
         } = newFlags;
 
         // setup logger
@@ -55,10 +55,11 @@ class ExportCantoCsvCommand extends BaseCommand {
         const dom = require('xmldom').DOMParser;
 
         //The uber json result object which will store the structured content for inserting to the CSV metadata
+        let assetlist = [];
         let assetjson = { metadata: {} };
 
         let node = null;
-        let xml = fs.readFileSync('./single_canto_asset_record.xml', 'utf8').toString();
+        let xml = fs.readFileSync(inputxml, 'utf8').toString();
         let doc = new dom().parseFromString(xml)
 
         // Parse the xml field name list using xmldom
@@ -86,9 +87,10 @@ class ExportCantoCsvCommand extends BaseCommand {
             node = result.iterateNext();
         }
 
+
         // Populate with metadata from the Canto Asset XML
         let metadataresult = xpath.evaluate(
-            "//*[local-name(.)='Item']", // xpathExpression
+            "//*[local-name(.)='Items']", // xpathExpression
             doc,                                   // contextNode
             null,                          // namespaceResolver
             xpath.XPathResult.ANY_TYPE,            // resultType
@@ -102,41 +104,51 @@ class ExportCantoCsvCommand extends BaseCommand {
             let childnodes = Array.from(node.childNodes)
             childnodes.forEach(item => {
                 if (item.nodeType == 1) {
-                    //console.log("uid:" + item.attributes.getNamedItem("uid").value);
-                    //console.log("   value:" + item.firstChild.nodeValue);
+                    let initialisedAssetJson = assetjson;
+                    let fieldValueNodes = Array.from(item.childNodes)
+                    fieldValueNodes.forEach(fieldvalue => {
+                        if (fieldvalue.nodeType == 1) {
+                            //console.log("uid:" + item.attributes.getNamedItem("uid").value);
+                            //console.log("   value:" + item.firstChild.nodeValue);
 
-                    let uid = item.attributes.getNamedItem("uid").value;
-                    let value = item.firstChild.nodeValue;
+                            let uid = fieldvalue.attributes.getNamedItem("uid").value;
+                            let value = fieldvalue.firstChild.nodeValue;
 
-                    if (value !== undefined && assetjson.metadata[uid] !== undefined) {
-                        let itemchildnodes = Array.from(item.childNodes)
+                            if (value !== undefined && initialisedAssetJson.metadata[uid] !== undefined) {
+                                let itemchildnodes = Array.from(fieldvalue.childNodes)
 
-                        if (itemchildnodes.length > 3) { // manage fields with more than one value
-                            //console.log(itemchildnodes)
-                            let multiValues = [];
-                            itemchildnodes.forEach(item => {
-                                if (item.nodeType == 1) {
-                                    multiValues.push(item.firstChild.nodeValue);
-                                    //console.log("name:" + item.firstChild.nodeValue);
+                                if (itemchildnodes.length > 3) { // manage fields with more than one value
+                                    //console.log(itemchildnodes)
+                                    let multiValues = [];
+                                    itemchildnodes.forEach(item => {
+                                        if (item.nodeType == 1) {
+                                            multiValues.push(item.firstChild.nodeValue);
+                                            //console.log("name:" + item.firstChild.nodeValue);
+                                        }
+                                    });
+                                    value = multiValues;
+                                } else {
+                                    itemchildnodes.forEach(item => {
+                                        if (item.nodeType == 1) {
+                                            value = item.firstChild.nodeValue;
+                                            //console.log("name:" + item.firstChild.nodeValue);
+                                        }
+                                    })
                                 }
-                            });
-                            value = multiValues;
-                        } else {
-                            itemchildnodes.forEach(item => {
-                                if (item.nodeType == 1) {
-                                    value = item.firstChild.nodeValue;
-                                    //console.log("name:" + item.firstChild.nodeValue);
-                                }
-                            })
+                                initialisedAssetJson.metadata[uid].value = value;
+                            }
                         }
-                        assetjson.metadata[uid].value = value;
-                    }
+                    });
+                    // The most succinct approach for creating a deep copy I could fine.
+                    // This is required to prevent all objs in the arrays being updated to the same value due to references
+                    const deepCopy = JSON.parse(JSON.stringify(initialisedAssetJson));
+                    assetlist.push(deepCopy);
                 }
             })
             node = metadataresult.iterateNext();
         }
 
-        console.log(assetjson);
+        console.log(assetlist);
         // node.forEach(parent => {
         //     console.log(parent);
         // })
